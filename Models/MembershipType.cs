567e375df965037c845a143555f6701f880656a9
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-05-08 20:32:43 +1000

    Alter Membership table :id set to int and data populated

--- Models/MembershipType.cs
+++ Models/MembershipType.cs
@@ -7,7 +7,7 @@ namespace MovieRental.Models
 {
     public class MembershipType
     {
-        public byte Id { get; set; }
+        public int Id { get; set; }
         public short SignUpFee { get; set; }
         //largest number is 12 here for 12 months. so, lets use byte
         public byte DurationInMonths { get; set; }
