d5e47a40e95db06b677177b28e3384e4aeb20b80
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-05-08 13:20:34 +1000

    Controller and View classes for Customer(model) added

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using MovieRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieRental.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ViewResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+            new Customer{Id=1,Name="James Williams"},
+            new Customer{Id=2,Name="John Smith"}
+            };
+        }
+    }
+}
