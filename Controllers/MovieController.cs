1d8d457a46220e50d2dc4604335749dc219e5824
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-05-08 11:06:44 +1000

    MVC in Action

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using MovieRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieRental.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult AllMovies()
+        {
+            var movie = new Movie () { Name = "Kung Fu Panda" };
+            return View(movie);
+        }
+    }
+}
