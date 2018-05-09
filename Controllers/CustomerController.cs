ba6aa095e836d94a6f6125983c61c1906086b9a0
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-05-09 10:58:26 +1000

    Revert "Load customer data from database table."
    This reverts commit 56b9396c26f22e04297337159d2aca1b2ca56460.

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -9,24 +9,20 @@ namespace MovieRental.Controllers
 {
     public class CustomerController : Controller
     {
-        private ApplicationDbContext _context;
-
-        public CustomerController()
-        {
-            _context = new ApplicationDbContext();
-        }
-        protected override void Dispose(bool disposing)
-        {
-            _context.Dispose();    
-        }
-
-        // Index method to show all customers from Customer table
+        // Index method to show all customers
         public ViewResult Index()
         {
-            var customers = _context.Customers.ToList();
+            var customers = GetCustomers();
             return View(customers);
         }
-
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+            new Customer{Id=1,Name="James Williams"},
+            new Customer{Id=2,Name="John Smith"}
+            };
+        }
         // Details method to show details of each customer id
         public ActionResult Details(int id)
         {
@@ -34,7 +30,7 @@ namespace MovieRental.Controllers
              * SingleOrDefault - Returns the only element of a sequence, or a default value if the sequence is empty; 
              * this method throws an exception if there is more than one element in the sequence.
             */
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
