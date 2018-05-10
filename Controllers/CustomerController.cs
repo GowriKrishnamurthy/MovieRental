f4706509ae5b3bcd7c57dbec2063db72f4d6daaf
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-05-10 21:50:17 +1000

    New form to add/edit Customer Details. Partial classes for Buttons- bootstrap

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -46,5 +46,63 @@ namespace MovieRental.Controllers
             return View(customer);
         }
 
+        // On click of New customer button, this method gets called to populate page with defaults
+        public ActionResult New()
+        {
+            var membershipType = _context.MembershipTypes.ToList();
+
+            // Create a Viewmodel as we need a view that combines 2 entities - MembershipType and Customer
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = new Customer(),
+                MembershipType = membershipType
+            };
+            return View("CustomerForm", viewModel);
+        }
+        
+        [HttpPost]
+        //This method gets called when Save button is clicked on new customer form
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+            {
+                //New customer
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                //existing customer - Edit changes
+                //Single method used - throws exception if  customer was not found with this ID
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate= customer.Birthdate;
+                customerInDb.MembershipTypeId= customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+
+                //Alternative options to save to Db
+                //1. TryUpdateModel(customerInDb); //Security issues may be come up
+                //2. Mapper.map(customer, customerInDb);
+            }
+            _context.SaveChanges();
+
+            //Once the changes from DB contexts are committed to DB, Home page of customer gets loaded
+            return RedirectToAction("Index", "Customer");
+        }
+        // On click of Edit customer button, this method gets called
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+ 
+            if (customer== null)
+                return HttpNotFound();
+
+            // Create a Viewmodel as we need a view that combines 2 entities - MembershipType and Customer
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipType = _context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm", viewModel);
+        }
     }
 }
