e57c31024c145cf0b74681c93ede5ba1c10991cd
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-05-10 14:25:34 +1000

    Movies data from DB. Details page added to each movie id

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -5,27 +5,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Data.Entity;
 namespace MovieRental.Controllers
 {
     public class MovieController : Controller
     {
-        //Adding a new view result to be called from Navbar link
+        //Adding a new view result to be called from Navbar link
+        private ApplicationDbContext _context;
+        public MovieController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // Index method to show all movies from table
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            // Include data from 2 tables - Movie and Genre
+            var movies = _context.Movies.Include(g => g.Genre).ToList();
 
             return View(movies);
         }
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Kung Fu Panda Part 1" },
-                new Movie { Id = 2, Name = "Avengers" }
-            };
-        }
 
+        // Details method to show details of each movie id
+        public ActionResult Details(int id)
+        {
+            /* 
+             * SingleOrDefault - Returns the only element of a sequence, or a default value if the sequence is empty; 
+             * this method throws an exception if there is more than one element in the sequence.
+            */
+            var movies = _context.Movies.Include(m => m.Genre).SingleOrDefault(c => c.Id == id);
+
+            if (movies == null)
+                return HttpNotFound();
+
+            return View(movies);
+        }
+        
         // GET: Movie
         public ActionResult AllMovies()
         {
