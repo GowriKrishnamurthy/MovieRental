c1220f3fdb45ae50ddc99953abeed57e8ff82507
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-05-12 13:25:30 +1000

    Automapper added for Customer APIs

---
+++ App_Start/MappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MovieRental.Dtos;
+using MovieRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieRental.App_Start
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            // Customer and Customer DTO should  be mappable to each other
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+
+            // Dto to Domain - Update fails as DTO to Customer, updates ID field too
+            // While Mapping objects, for member ID, set an option to be implemented.
+            // Option defined here for ID is to just ignore..
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+        }
+    }
+}
--- Global.asax.cs
+++ Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using MovieRental.Models;
+﻿using AutoMapper;
+using MovieRental.App_Start;
+using MovieRental.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -9,12 +11,16 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+
 namespace MovieRental
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            // Initialize AutoMapper
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+
             // Added to configure WebAPI
             GlobalConfiguration.Configure(WebApiConfig.Register);
             Database.SetInitializer<ApplicationDbContext>(null); //Disable initializer
